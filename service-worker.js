73ee528a6100f996964f664ad9c104371eab5876
Suraj Patel <surajdantod@gmail.com>
2024-10-07 21:30:40 +0530

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'growth-app-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'style.css', // If you have a separate CSS file
+  'icon-192x192.png',
+  'icon-512x512.png',
+  // Add any other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return the cached response if available, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
