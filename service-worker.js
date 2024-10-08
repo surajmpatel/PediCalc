bb4bada6207c20b5b887f372bbd1a4ccff72bf67
Suraj Patel <surajdantod@gmail.com>
2024-10-08 18:32:11 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,42 +2,29 @@ const CACHE_NAME = 'growth-app-cache-v1';
 const urlsToCache = [
   'index.html',
   'manifest.json',
-  'style.css', // If you have a separate CSS file
-  'icon-192x192.png',
-  'icon-512x512.png',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
   // Add any other assets you want to cache
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Return the cached response if available, otherwise fetch from network
-        return response || fetch(event.request);
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
